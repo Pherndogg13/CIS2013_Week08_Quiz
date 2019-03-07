2d14039908b6904de09d51d763cbb4c6a966fc9b
Bob Rockers <rockers@okstate.edu>
2019-03-06 18:12:42 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int LEN;
+	
+	cout << "Enter monetary vaule " << endl;
+	cin >> LEN;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
