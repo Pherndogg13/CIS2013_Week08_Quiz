548fd7249d8dcdb4ba9ac021605564ca962c9e66
Bob Rockers <rockers@okstate.edu>
2019-03-06 18:23:04 -0600

    added menu for week 8 quiz
    added a menu for quiz

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(){
 	cout << "Enter monetary vaule " << endl;
 	cin >> LEN;
 	
+	cout << " a)show the user a 15% tip amountb) Show the user the equivalent amount in quaters, nickels, dimes, and pennies using the minimum physical change count.c) ask the user for an exchange rate, and print the value of the moeny in that exchange rate." << endl;
+ 
 	
 	
 	
